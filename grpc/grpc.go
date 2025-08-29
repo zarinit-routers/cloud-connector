d27592d29c02463b2004a59c19eadfae1d20b6c5
Katy248 <petrovanton247@gmail.com>
2025-08-29 13:20:21 +0300

    Add tags

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -44,11 +44,13 @@ func (s *clientsService) GetClients(ctx context.Context, req *pb.GetClientsReque
 		&pb.Client{
 			Id:   "1",
 			Name: "Dummy Client 1",
+			Tags: []string{"dummy", "cool", "first"},
 		})
 	response.Clients = append(response.Clients,
 		&pb.Client{
 			Id:   "2",
 			Name: "Dummy Client 2",
+			Tags: []string{"dummy", "metal", "pop"},
 		})
 	return response, nil
 }
