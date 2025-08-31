6effb978c477b5d6a56ac5232cf58fb2344cf752
Katy248 <petrovanton247@gmail.com>
2025-08-31 15:48:39 +0300

    Added database package

--- storage/database/database.go
+++ storage/database/database.go
@@ -3,12 +3,14 @@ package database
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/log"
 )
 
 const (
 	ENV_CONNECTION_STRING = "DATABASE_CONNECTION_STRING"
+	ENV_AUTO_MIGRATE      = "DATABASE_AUTO_MIGRATE"
 )
 
 func getConnectionString() (string, error) {
@@ -19,11 +21,32 @@ func getConnectionString() (string, error) {
 
 	return conn, nil
 }
+
+func shouldMigrate() bool {
+	envVar := strings.ToLower(os.Getenv(ENV_AUTO_MIGRATE))
+	if envVar == "1" || envVar == "yes" || envVar == "true" {
+		return true
+	}
+	return false
+}
+
+func migrate() error {
+	log.Info("Migrating database")
+	return fmt.Errorf("migrations not implemented")
+}
+
 func Setup() error {
 	conn, err := getConnectionString()
 	if err != nil {
 		return fmt.Errorf("bad connection string: %s", err)
 	}
 	log.Info("Connecting to database", "connectionString", conn)
+
+	if shouldMigrate() {
+		if err := migrate(); err != nil {
+			return fmt.Errorf("migrations failed: %s", err)
+		}
+	}
+
 	return nil
 }
